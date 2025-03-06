989b5b88de8f7d9de5a3293441c5e32305099499
Daniel Umberto <danielumberto51@gmail.com>
2025-03-06 15:20:52 -0300

    initial commit

---
+++ Src/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext("2d");
+
+// Ajusta o tamanho do Canvas(Provavelmente será mudada no futuro essas medidas)
+canvas.width = 1024;
+canvas.height = 576;
+
