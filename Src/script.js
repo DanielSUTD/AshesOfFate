6fa0e2d228a6e545477ef4b67a8839f30cad48da
Daniel Umberto <danielumberto51@gmail.com>
2025-04-06 20:03:08 -0300

    feat: Tela inicial e de História

--- Src/script.js
+++ Src/script.js
@@ -315,10 +315,6 @@ function animate(){
 }
 
 
-//Chama a função de animação
-animate()
-
-
 //Movimentação com 
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
@@ -357,3 +353,50 @@ window.addEventListener('keyup', (e) => {
             break
     }
 })
+
+
+// História do jogo
+const story = `
+Gael Thorne, um homem consumido pela dor, parte em busca dos lendários Três Artefatos de Terra Desconhecida(Ilhas de Orlath) para ressuscitar sua esposa, morta por uma doença misteriosa.
+`;
+
+//Efeito de digitação letra por letra
+function typeWriterEffect(text, element, speed, callback) {
+    let i = 0;
+    let interval = setInterval(() => {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+        } else {
+            clearInterval(interval);
+            if (callback) callback();
+        }
+    }, speed);
+}
+
+// Transição entre telas
+setTimeout(() => {
+    document.getElementById("introScreen").style.animation = "none";
+    document.getElementById("introScreen").style.opacity = "1";
+    document.getElementById("initButton").style.display = "flex";
+}, 4000);
+
+
+function showStory() {
+    document.getElementById("introScreen").style.display = "none";
+    document.getElementById("storyScreen").style.display = "flex";
+
+    const storyText = document.getElementById("storyText");
+    typeWriterEffect(story, storyText, 50, () => {
+        document.getElementById("startButton").style.display = "block";
+    });
+}
+
+function startGame() {
+    document.getElementById("storyScreen").style.display = "none";
+    document.getElementById("game-canvas").style.display = "block";
+
+    
+    //Responsável por animar o jogo
+    animate();
+}
