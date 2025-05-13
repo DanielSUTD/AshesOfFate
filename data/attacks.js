5bcaa78596a1833aeb678369c7877ab36b822759
Daniel Umberto <danielumberto51@gmail.com>
2025-05-12 21:10:00 -0300

    Arquivo responsável pelos ataques

---
+++ data/attacks.js
@@ -0,0 +1,12 @@
+const attacks = {
+    QuickSlash: {
+        name: 'Quick Slash',
+        damage: 10,
+        type: 'Normal'
+    },
+    LightningBlade: {
+        name: 'Lightning Blade',
+        damage: 25,
+        type: 'Electric'
+    }
+}
